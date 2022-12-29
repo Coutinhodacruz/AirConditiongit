ff8ba53ddc86f89a232d9025b67c4be1e66a6638
onukwufor dominic <dominicrotimi@gmail.com>
2022-12-28 23:38:15 -0500

    xmas snacks

---
+++ Assignment/src/XmasSnacks/SalaryCalculator.java
@@ -0,0 +1,83 @@
+package XmasSnacks;
+
+
+import java.util.Scanner;
+
+public class SalaryCalculator {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+        String[] EmployeeNames = {" ", " ", " "};
+        double HoursWorked = 0;
+        double hourlyRate;
+        double gross = 0;
+        int numberOfHours = 0;
+
+        System.out.println("Welcome to the Salary Calculator.\n");
+        System.out.println("Please enter:");
+
+        for (int i = 0; i < 3; i++) {
+            System.out.println("Employee" + (i + 1) + " Name: ");
+            EmployeeNames[i] = scanner.nextLine();
+        }
+
+        for (int i = 0; i < 3; i++) {
+            System.out.print(EmployeeNames[i] + "'s hours worked: ");
+            HoursWorked = scanner.nextInt();
+        }
+
+        System.out.print("and finally, how much all three made per hour: ");
+        hourlyRate = scanner.nextDouble();
+        for (int i = 0; i < 3; i++) {
+
+            gross = HoursWorked * hourlyRate;
+                gross = (numberOfHours + HoursWorked) * hourlyRate;
+                System.out.printf("The gross pay for " + EmployeeNames[i] + " is: $%.2f\n", hourlyRate * HoursWorked + numberOfHours);
+
+
+        }
+
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//while (true) {
+//        System.out.println("Name(empty - Exit):");
+//        String name = in.nextLine();
+//        if(name.length()==0){
+//        break;
+//        }
+//        System.out.println("Hours");
+//        int hours = Integer.parseInt(in.nextLine());
+//        System.out.println("Rate:");
+//        double rate = Double.parseDouble(in.nextLine());
+//        System.out.println(name + " Gross: " + (hours <= 40 ? rate * hours : (rate * 40) + (rate * (hours - 40) * 1.5)));
+
+
+
